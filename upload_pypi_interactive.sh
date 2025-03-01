dbea3597b1a056384fda9f19e32efc056168e98d
wsqstar <wsq_star@outlook.com>
2025-03-01 09:11:53 +0000

    update

--- upload_pypi_interactive.sh
+++ upload_pypi_interactive.sh
@@ -17,8 +17,8 @@ echo "Checking package metadata with Twine..."
 twine check dist/*
 
 # Prompt for upload target
-read -p "Do you want to upload to (1) PyPI or (2) TestPyPI? (Enter 1 or 2): " choice
-
+echo "Do you want to upload to (1) PyPI or (2) TestPyPI? (Enter 1 or 2):"
+read choice
 if [[ "$choice" -eq 1 ]]; then
     echo "Uploading to PyPI..."
     python3 -m twine upload dist/*
